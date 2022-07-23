72046a732e0cdf048e1359f26f8a82fa5502084b
YehonatanHen <yonatan.h333@gmail.com>
2022-07-23 18:25:06 +0300

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from './screens/mainPage';
 import { WelcomePage } from './screens/welcomePage'
+import '../src/css/app.css'
 
 function App() {
 
